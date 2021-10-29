ea2a24d98f6acf047a31f3b30455fc071598e3dd
treyturley <81778542+treyturley@users.noreply.github.com>
2021-10-28 23:06:05 -0500

    implemented sorting

--- NMS-Cinema-UX/src/app/browse/browse.component.ts
+++ NMS-Cinema-UX/src/app/browse/browse.component.ts
@@ -20,6 +20,7 @@ export class BrowseComponent implements OnInit {
   genres: Array<Genre> = new Array<Genre>();
   searchTerm: string = "";
   isGenreFilterApplied: boolean = false;
+  sortBy: any[] = ["Genre", "A to Z",]
 
   time: string = "";
   quantity: number = 0;
@@ -44,17 +45,19 @@ export class BrowseComponent implements OnInit {
   getMovies() {
     this._movieSvc.getMovies().subscribe(
       result => {
-        // FIX: This throws an error when the result is an empty list
-        result.forEach(movie => {
-          if(movie.active){
-            this.allMovies.push(movie);
-          }
-        });
-        this.movies=this.allMovies;
+        if (result.length != 0) {
+          result.forEach(movie => {
+            if (movie.active) {
+              this.allMovies.push(movie);
+            }
+          });
+          this.movies = this.allMovies;
+        }
       },
       error => {
         console.log("error occured while getting movies");
       });
+
   }
 
   getGenres() {
@@ -74,8 +77,8 @@ export class BrowseComponent implements OnInit {
     cartItem.movie = movie;
     cartItem.showtime = form.value.showtime;
     cartItem.numTickets = parseInt(form.value.tickets);
-    cartItem.subTotal = cartItem.numTickets * movie.ticketPrice;    
-    
+    cartItem.subTotal = cartItem.numTickets * movie.ticketPrice;
+
     this._cartSvc.addToCart(cartItem);
 
     this._cartSvc.saveCart();
@@ -111,11 +114,11 @@ export class BrowseComponent implements OnInit {
     this.movies = movieSubset;
   }
 
-  clearGenreFilter(){
-    this.movies=this.allMovies;
+  clearGenreFilter() {
+    this.movies = this.allMovies;
   }
 
-  filterMovies(searchStr:string){
+  filterMovies(searchStr: string) {
     let movieSubset = new Array<Movie>();
 
     //if searchTerm not empty
@@ -127,10 +130,11 @@ export class BrowseComponent implements OnInit {
       });
       //console.log(productSubset);
       this.movies = movieSubset;
+      this.searchTerm = "";
     }
     else {
       this.searchTerm = "";
-      this.movies=this.allMovies;
+      this.movies = this.allMovies;
     }
   }
 
@@ -144,6 +148,32 @@ export class BrowseComponent implements OnInit {
 
   getShowtimesFromMovie(movie: Movie): Array<string> {
     return movie.showtimes.split(",");
+  }
+
+  sortMoviesBy(sortOption: string, elmt: HTMLSelectElement) {
+    console.log("sort by: " + sortOption);
+
+    if (this.movies.length > 1) {
+
+      if (sortOption == "Genre") {
+        this.movies.sort((a, b) => this.getGenreName(a.genreId)! < this.getGenreName(b.genreId)! ? -1 : this.getGenreName(a.genreId)! > this.getGenreName(b.genreId)! ? 1 : 0)
+        elmt.selectedIndex = 0;
+      }
+      else if (sortOption == "A to Z") {
+        this.movies.sort((a, b) => a.title < b.title ? -1 : a.title > b.title ? 1 : 0);
+        elmt.selectedIndex = 0;
+      }
+      else {
+        this.movies = this.allMovies;
+        elmt.selectedIndex = 0;
+      }
+    }
+
+  }
+
+  resetSort(elmt: HTMLSelectElement) {
+    elmt.selectedIndex = 0;
+    this.movies = this.allMovies;
   }
 
 }
